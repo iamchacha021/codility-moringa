0ce225ec75c4381d65599e5612f1f98ff63015a7
iamchacha021 <iamchacha021@gmail.com>
2023-02-23 14:46:02 +0300

    Complete challenge two

--- challenge_one.rb
+++ challenge_one.rb
@@ -1,4 +1,13 @@
 def equlity(num1, num2)
-    
+    if num1 > num2
+        "#{num1} is greater than #{num2}"
+    end
+    elsif num1 < num2
+        "#{num1} is less than #{num2}"
+    elsif num1 == num2
+        "#{num1} is equal to #{num2}"
+    else
+        "Please input a valid number"
+    end
 
 end
--- challenge_two.rb
+++ challenge_two.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def count_occurrence(string)
-    
+    str_arr = []
+
+    split_str = string.split("")
+
+    split_str.each do |s|
+        str_count = split_str.count(s)
+        str_arr << [s, str_count]
+    end
+    str_arr.uniq
 end
+
+count_occurrence("bbaa")
+binding.pry
