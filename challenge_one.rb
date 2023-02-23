4609567dcc6d8118383f646d100c7e2dddde6a85
iamchacha021 <iamchacha021@gmail.com>
2023-02-23 15:03:23 +0300

    attempt challenge one

--- challenge_one.rb
+++ challenge_one.rb
@@ -1,7 +1,15 @@
-def equlity(num1, num2)
+require 'pry'
+
+def equality(num1, num2)
+    num_arr = [num1, num2]
+    num_arr.sort do |num1, num2|
+        num1 <=> num2
+    end
+end
+
+def equality_if(num1, num2)
     if num1 > num2
         "#{num1} is greater than #{num2}"
-    end
     elsif num1 < num2
         "#{num1} is less than #{num2}"
     elsif num1 == num2
@@ -11,3 +19,5 @@ def equlity(num1, num2)
     end
 
 end
+
+binding.pry
