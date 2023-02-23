1e0d8c80df76504bf102d80b195b7ede456fde15
iamchacha021 <iamchacha021@gmail.com>
2023-02-23 15:39:25 +0300

    Update equality method

--- challenge_one.rb
+++ challenge_one.rb
@@ -1,9 +1,13 @@
 require 'pry'
 
 def equality(num1, num2)
-    num_arr = [num1, num2]
-    num_arr.sort do
-        num1 <=> num2
+    case num1 <=> num2
+        when 1
+            "#{num1} is greater than #{num2}"
+        when -1
+            "#{num1} is less than #{num2}"
+        else
+            "#{num1} is equal to #{num2}" 
     end
 end
 
